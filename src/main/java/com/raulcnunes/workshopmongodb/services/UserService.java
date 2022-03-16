c0a8da65940f44c37df66471a0f8268903c7f97e
Raul <rcavedonnunes@gmail.com>
2022-03-16 00:03:04 -0300

    Obtendo um usuario por ID

--- src/main/java/com/raulcnunes/workshopmongodb/services/UserService.java
+++ src/main/java/com/raulcnunes/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.raulcnunes.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.raulcnunes.workshopmongodb.domain.User;
 import com.raulcnunes.workshopmongodb.repository.UserRepository;
+import com.raulcnunes.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){	
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
